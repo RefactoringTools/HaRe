08020fbb47290f45dfbed6a97ad080c60979e4d2
Alan Zimmerman <alan.zimm@gmail.com>
2012-08-07 21:34:53 +0200

    Simple whitespace adjustments to pass tests

--- testing/addCon/A1.hs
+++ testing/addCon/A1.hs
@@ -1,10 +1,8 @@
 module A1 where
 
-data T c a = C1 a | C2 c  
+data T a = C1 a
 
 
-addedC2 = error "added C2 c to T"
-over :: (T c b) -> b
+over :: (T b) -> b
 over (C1 x) = x
-over (C2 a) = addedC2
 
--- testing/addCon/B2_TokOut.hs
+++ testing/addCon/B2_TokOut.hs
@@ -4,6 +4,7 @@ data Data1 a = C1 a Int Int  | C4 Float  | C2 Int | C3 Float
 
 
 addedC4 = error "added C4 Float to Data1"
+
 g (C1 x y z) (C1 n m o) = y + m
 g (C4 a) b = addedC4
 g a (C4 b) = addedC4
