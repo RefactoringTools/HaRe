7a309786567c6c47ba6da552071f6eeb6b4521d8
Alan Zimmerman <alan.zimm@gmail.com>
2013-08-26 20:26:29 +0200

    WIP

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cabal clean && cabal configure --enable-tests --enable-library-coverage -fhpc && cabal build && cabal test
+cabal clean && cabal configure --enable-tests --enable-library-coverage -fhpc --enable-library-profiling --enable-executable-profiling && cabal build && cabal test
 #cabal clean && cabal configure --enable-tests && cabal build && cabal test
 #mkdir ./dist/coverage
 #hpc report  ./dist/hpc/tix/spec/spec.tix
