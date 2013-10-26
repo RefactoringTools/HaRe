7a4d545d8ac8987a2a9bc1ad7f4f2e8051a9b197
Alan Zimmerman <alanz@localhost>
2013-10-26 22:18:19 +0200

    WIP.

--- src/Language/Haskell/Refact/Utils/Layout.hs
+++ src/Language/Haskell/Refact/Utils/Layout.hs
@@ -2,6 +2,7 @@
 --
 
 module Language.Haskell.Refact.Utils.Layout (
+  allocTokens
   ) where
 
 import qualified GHC           as GHC
@@ -9,9 +10,10 @@ import qualified GHC           as GHC
 import qualified Data.Generics as SYB
 import qualified GHC.SYB.Utils as SYB
 
-import Data.Tree
+import Data.List
 import Language.Haskell.Refact.Utils.GhcUtils
 import Language.Haskell.Refact.Utils.LocUtils
+import Language.Haskell.Refact.Utils.TokenUtils
 import Language.Haskell.Refact.Utils.TypeSyn
 
 -- ---------------------------------------------------------------------
@@ -122,21 +124,22 @@ type RowOffset = Int
 type ColOffset = Int
 data Layout = Above [Layout]
             | Offset RowOffset ColOffset Layout
-            | Leaf [PosToken]
+            | Leaf                    [PosToken]
+            | LeafLocated GHC.SrcSpan [PosToken]
             deriving (Show)
 
 -- ---------------------------------------------------------------------
 
 
-allocateTokens :: GHC.RenamedSource -> [PosToken] -> Layout
-allocateTokens = undefined
+allocateTokens :: GHC.ParsedSource -> [PosToken] -> Layout
+allocateTokens = error "allocateTokens undefined"
 
 -- |Perform a top-down traversal of the AST fragment building up the Layout tree in the process.
 -- NOTE: may need to use ParsedSource, it is in lexical order
 allocateTokensToAST :: (SYB.Data t) => t -> [PosToken] -> [Layout]
 allocateTokensToAST t toks = r
   where
-    r = everythingStaged SYB.Renamer (++) []
+    r = everythingStaged SYB.Parser (++) []
       ([] `SYB.mkQ` allocateLet
 --          `SYB.extQ` allocateWhere
 --          `SYB.extQ` allocateOf
@@ -171,8 +174,234 @@ So we would expect a result as
 , Offset 0 1 (alloc expr exprtoks)
 ]
 
-
-
-
-
 -}
+
+-- TODO: bring in startEndLocIncComments'
+allocTokens :: GHC.ParsedSource -> [PosToken] -> [Layout]
+allocTokens (GHC.L _l (GHC.HsModule maybeName maybeExports imports decls _warns _haddocks)) toks = r
+  where
+    (nameLayout,toks1) =
+      case maybeName of
+        Nothing -> ([],toks)
+        Just (GHC.L ln _modName) -> ([Leaf s1,LeafLocated ln modNameToks],toks')
+          where
+            (s1,modNameToks,toks') = splitToks (ghcSpanStartEnd ln) toks
+
+    (exportLayout,toks2) =
+      case maybeExports of
+        Nothing -> ([],toks1)
+        Just exps -> ([Leaf s2,Leaf expToks],toks2')
+          where
+            (s2,expToks,toks2') = splitToksForList exps toks1
+
+    (importLayout,toks3) =
+      case imports of
+        [] -> ([],toks2)
+        is -> ([Leaf s3,Leaf impToks],toks3')
+          where
+            (s3,impToks,toks3') = splitToksForList is toks2
+
+    (declLayout,_toks4) =
+      case decls of
+        [] -> ([],toks3)
+        -- is -> ([Leaf s4,Leaf impToks,Leaf toks4'],[])
+        is -> ([Leaf s4] ++ allocDecls is declToks ++ [Leaf toks4'],[])
+          where
+            (s4,declToks,toks4') = splitToksForList is toks3
+
+
+    r = strip $ nameLayout ++ exportLayout ++ importLayout ++ declLayout
+
+-- ---------------------------------------------------------------------
+
+allocDecls :: [GHC.LHsDecl GHC.RdrName] -> [PosToken] -> [Layout]
+allocDecls decls toks = r
+  where
+    (declLayout,tailToks) = foldl' doOne ([],toks) decls
+
+    r = strip $ declLayout ++ [Leaf tailToks]
+
+    doOne :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+    doOne acc d@(GHC.L _ (GHC.TyClD       _)) = allocTyClD       acc d
+    doOne acc d@(GHC.L _ (GHC.InstD       _)) = allocInstD       acc d
+    doOne acc d@(GHC.L _ (GHC.DerivD      _)) = allocDerivD      acc d
+    doOne acc d@(GHC.L _ (GHC.ValD        _)) = allocValD        acc d
+    doOne acc d@(GHC.L _ (GHC.SigD        _)) = allocSigD        acc d
+    doOne acc d@(GHC.L _ (GHC.DefD        _)) = allocDefD        acc d
+    doOne acc d@(GHC.L _ (GHC.ForD        _)) = allocForD        acc d
+    doOne acc d@(GHC.L _ (GHC.WarningD    _)) = allocWarningD    acc d
+    doOne acc d@(GHC.L _ (GHC.AnnD        _)) = allocAnnD        acc d
+    doOne acc d@(GHC.L _ (GHC.RuleD       _)) = allocRuleD       acc d
+    doOne acc d@(GHC.L _ (GHC.VectD       _)) = allocVectD       acc d
+    doOne acc d@(GHC.L _ (GHC.SpliceD     _)) = allocSpliceD     acc d
+    doOne acc d@(GHC.L _ (GHC.DocD        _)) = allocDocD        acc d
+    doOne acc d@(GHC.L _ (GHC.QuasiQuoteD _)) = allocQuasiQuoteD acc d
+
+-- ---------------------------------------------------------------------
+
+allocTyClD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocTyClD (acc,toks) d@(GHC.L l (GHC.TyClD       _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocInstD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocInstD (acc,toks) d@(GHC.L l (GHC.InstD       _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocDerivD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocDerivD (acc,toks) d@(GHC.L l (GHC.DerivD      _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocValD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocValD (acc,toks) (GHC.L _l (GHC.ValD (GHC.FunBind (GHC.L ln _) _ (GHC.MatchGroup matches _) _ _ _))) = r
+  where
+    (nameLayout,toks1) = ([Leaf s1,LeafLocated ln nameToks],toks')
+      where
+        (s1,nameToks,toks') = splitToks (ghcSpanStartEnd ln) toks
+
+    (matchesLayout,toks2) = ([Leaf s2] ++ allocMatches matches matchToks,toks2')
+          where
+            (s2,matchToks,toks2') = splitToksForList matches toks1
+
+    r = (acc ++ (strip $ nameLayout ++ matchesLayout),toks2)
+
+allocValD (acc,toks) d@(GHC.L l (GHC.ValD (GHC.PatBind lhs rhs _ _ _))) = r
+  where
+    r = ([],[])
+
+allocValD (acc,toks) d@(GHC.L l (GHC.ValD (GHC.VarBind n rhs _))) = error "allocValD:VarBinds"
+allocValD (acc,toks) d@(GHC.L l (GHC.ValD (GHC.AbsBinds tvs vars exps ev binds))) = error "allocValD:AbsBinds"
+
+-- ---------------------------------------------------------------------
+
+allocSigD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocSigD (acc,toks) d@(GHC.L l (GHC.SigD        _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocDefD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocDefD (acc,toks) d@(GHC.L l (GHC.DefD        _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocForD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocForD (acc,toks) d@(GHC.L l (GHC.ForD        _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocWarningD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocWarningD (acc,toks) d@(GHC.L l (GHC.WarningD    _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocAnnD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocAnnD (acc,toks) d@(GHC.L l (GHC.AnnD        _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocRuleD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocRuleD (acc,toks) d@(GHC.L l (GHC.RuleD       _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocVectD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocVectD (acc,toks) d@(GHC.L l (GHC.VectD       _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocSpliceD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocSpliceD (acc,toks) d@(GHC.L l (GHC.SpliceD     _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocDocD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocDocD (acc,toks) d@(GHC.L l (GHC.DocD        _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocQuasiQuoteD :: ([Layout],[PosToken]) -> GHC.LHsDecl GHC.RdrName -> ([Layout],[PosToken])
+allocQuasiQuoteD (acc,toks) d@(GHC.L l (GHC.QuasiQuoteD _)) = ([],[])
+
+-- ---------------------------------------------------------------------
+
+allocMatches :: [GHC.LMatch GHC.RdrName] -> [PosToken] -> [Layout]
+allocMatches matches toksIn = allocList matches toksIn doOne
+  where
+    doOne :: GHC.LMatch GHC.RdrName -> [PosToken] -> [Layout]
+    doOne (GHC.L _ (GHC.Match pats _mtyp (GHC.GRHSs rhs localBinds))) toks = r
+      where
+        (s1,patsToks,toks1) = splitToksForList pats toks
+        (s2,rhsToks,bindsToks) = splitToksForList rhs toks1
+        patLayout = allocList pats patsToks allocPat
+        rhsLayout = allocList rhs rhsToks allocRhs
+        localBindsLayout = allocLocalBinds localBinds bindsToks
+        r = (strip $ [Leaf s1] ++ patLayout ++ [Leaf s2] ++ rhsLayout ++ localBindsLayout)
+
+-- ---------------------------------------------------------------------
+
+allocPat :: GHC.LPat GHC.RdrName -> [PosToken] -> [Layout]
+allocPat (GHC.L l _) toks = [Leaf toks]
+
+-- ---------------------------------------------------------------------
+
+allocRhs :: GHC.LGRHS GHC.RdrName -> [PosToken] -> [Layout]
+allocRhs (GHC.L l (GHC.GRHS stmts expr)) toksIn = r
+  where
+    r = error "allocRhs undefined"
+
+-- ---------------------------------------------------------------------
+
+allocLocalBinds :: GHC.HsLocalBinds GHC.RdrName -> [PosToken] -> [Layout]
+allocLocalBinds GHC.EmptyLocalBinds toks = strip $ [Leaf toks]
+allocLocalBinds (GHC.HsValBinds (GHC.ValBindsIn binds sigs)) toks = error "allocLocalBinds undefined"
+
+allocLocalBinds (GHC.HsIPBinds ib)  toks = error "allocLocalBinds undefined"
+
+-- ---------------------------------------------------------------------
+
+strip :: [Layout] -> [Layout]
+strip ls = filter notEmpty ls
+  where
+    notEmpty (Leaf []) = False
+    notEmpty (LeafLocated _ []) = False
+    notEmpty _ = True
+
+-- ---------------------------------------------------------------------
+
+-- TODO: work this in everywhere
+allocList :: [GHC.Located a] -> [PosToken] -> (GHC.Located a -> [PosToken] -> [Layout]) -> [Layout]
+allocList xs toksIn allocFunc = r
+  where
+    (layout,toks2) = ([Leaf s2] ++ allocAll xs listToks,toks2')
+          where
+            (s2,listToks,toks2') = splitToksForList xs toksIn
+
+    allocAll xs' toks = res
+      where
+        (declLayout,tailToks) = foldl' doOne ([],toks) xs'
+
+        res = strip $ declLayout ++ [Leaf tailToks]
+
+        -- doOne :: ([Layout],[PosToken]) -> GHC.Located a -> ([Layout],[PosToken])
+        doOne (acc,toksOne) x@(GHC.L l _) = r1
+          where
+            (s1,funcToks,toks') = splitToks (ghcSpanStartEnd l) toksOne
+            layout' = [Leaf s1] ++ allocFunc x funcToks
+            r1 = (acc ++ (strip $ layout'),toks')
+
+    r = strip $ layout ++ [Leaf toks2]
+
+-- ---------------------------------------------------------------------
+
+-- | Split the given tokens into the ones that occur prior to the start
+-- of the list and ones that occur after
+splitToksForList :: [GHC.Located a] -> [PosToken] -> ([PosToken],[PosToken],[PosToken])
+splitToksForList [] toks = ([],[],toks)
+splitToksForList xs toks = splitToks (getGhcLoc s, getGhcLocEnd e) toks
+  where
+    (GHC.L s _) = head xs
+    (GHC.L e _) = last xs
+
+-- ---------------------------------------------------------------------
---
+++ test/LayoutSpec.hs
@@ -0,0 +1,61 @@
+module LayoutSpec (main, spec) where
+
+import           Test.Hspec
+
+import qualified FastString as GHC
+import qualified GHC        as GHC
+import qualified Lexer      as GHC
+
+import qualified GHC.SYB.Utils as SYB
+
+import Control.Monad.State
+import Data.Maybe
+import Data.Tree
+
+import Language.Haskell.Refact.Utils.GhcVersionSpecific
+import Language.Haskell.Refact.Utils.LocUtils
+import Language.Haskell.Refact.Utils.Monad
+import Language.Haskell.Refact.Utils.TokenUtils
+import Language.Haskell.Refact.Utils.Layout
+import Language.Haskell.Refact.Utils.TokenUtilsTypes
+import Language.Haskell.Refact.Utils.TypeSyn
+import Language.Haskell.Refact.Utils.TypeUtils
+
+import qualified Data.Tree.Zipper as Z
+import qualified Data.Map as Map
+
+import TestUtils
+
+-- ---------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  hspec spec
+
+spec :: Spec
+spec = do
+
+  -- ---------------------------------------------
+
+  describe "allocTokens" $ do
+    it "allocates Tokens for a default main module" $ do
+      (t,toks) <- parsedFileBare
+      let parsed = GHC.pm_parsed_source $ GHC.tm_parsed_module t
+
+      -- (SYB.showData SYB.Parser 0 parsed) `shouldBe` ""
+
+      (show toks) `shouldBe`
+         "[((((3,1),(3,5)),ITvarid \"main\"),\"main\"),"++
+          "((((3,6),(3,7)),ITequal),\"=\"),"++
+          "((((3,8),(3,16)),ITvarid \"putStrLn\"),\"putStrLn\"),"++
+          "((((3,17),(3,24)),ITstring \"hello\"),\"\\\"hello\\\"\")]"
+
+      let layout = allocTokens parsed toks
+      (show layout) `shouldBe` ""
+
+-- ---------------------------------------------------------------------
+
+parsedFileBare :: IO (ParseResult,[PosToken])
+parsedFileBare = parsedFileGhc "./test/testdata/Layout/Bare.hs"
+
+-- ---------------------------------------------------------------------
