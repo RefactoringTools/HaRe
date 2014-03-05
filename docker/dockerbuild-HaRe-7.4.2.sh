4941de308e97fb776583f8417698fd638dbc5ca0
Alan Zimmerman <alanz@alanz-laptop>
2014-03-05 23:32:13 +0200

    Added some documentation to the docker file

--- docker/dockerbuild-HaRe-7.4.2.sh
+++ docker/dockerbuild-HaRe-7.4.2.sh
@@ -10,3 +10,9 @@ docker build -t alanz/HaRe-7.4.2-64 ./HaRe-7.4.2
 
 # Note: to debug the partial build do the following
 # docker run -i -t alanz/HaRe-7.4.2-64 /bin/bash
+
+# To run the final instance
+# docker run -d -P alanz/HaRe-7.4.2-64
+
+# get the port from `docker ps`
+# ssh -X -l docker localhost -p 49153
