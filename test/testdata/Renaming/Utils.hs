72cde4d5f935e1df0e88c08e1ba0320af1efc357
Alan Zimmerman <alanz@alanz-laptop>
2014-03-18 16:45:20 +0200

    Simplify test case

--- test/testdata/Renaming/Utils.hs
+++ test/testdata/Renaming/Utils.hs
@@ -1,35 +1,20 @@
-module Layout.Utils (main, spec) where
+module Layout.Utils where
 
-main :: IO ()
-main = do
-  hspec spec
-
-spec :: IO ()
 spec = do
 
-  describe "locToExp on ParsedSource" $ do
-    it "finds the largest leftmost expression contained in a given region #1" $ do
-      (t, _toks) <- parsedFileBGhc
-      let parsed = pm_parsed_source $ tm_parsed_module t
+  describe "description" $ do
+    it "blah" $ do
+      t <- parsedFileBGhc
+      let parsed = 3
 
-      let (Just expr) = locToExp (7,7) (7,43) parsed :: Maybe (Located (HsExpr RdrName))
-      getLocatedStart expr `shouldBe` (7,9)
-      getLocatedEnd   expr `shouldBe` (7,42)
+      let (Just expr) = Just 2
+      return ()
 
 
 parsedFileBGhc :: IO (Int,String)
 parsedFileBGhc = undefined
-tm_parsed_module = undefined
-pm_parsed_source = undefined
-locToExp = undefined
-getLocatedStart = undefined
-getLocatedEnd = undefined
 describe = undefined
 hspec = undefined
 it = undefined
-shouldBe = undefined
 
-data Located a = L a
-data HsExpr a = HsExpr a
-data RdrName = RdrName
-data Spec = Spec
+
