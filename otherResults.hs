9e51437e4ff62d9402874327869880f09941c4d8
Stephen A <StephenAdams.Minn@gmail.com>
2014-04-02 17:18:12 +0100

    Merge from upstream

---
+++ otherResults.hs
@@ -0,0 +1,14 @@
+import Data.Algorithm.Diff
+
+compareFiles :: FilePath -> FilePath -> IO [Diff [String]]
+compareFiles fileA fileB = do
+  astr <- readFile fileA
+  bstr <- readFile fileB
+  -- return $ filter (\c -> not( isBoth c)) $ getGroupedDiff (lines astr) (lines bstr)
+  return $ compareStrings astr bstr
+
+compareStrings :: String -> String -> [Diff [String]]
+compareStrings cstr bstr = filter (\c -> not( isBoth c)) $ getGroupedDiff (lines cstr) (lines bstr)
+    where
+      isBoth (Both _ _) = True
+      isBoth _        = False
