083795391ef151d19a287195858873af262abc64
Stephen Adams <sa597@kent.ac.uk>
2014-10-07 11:32:44 +0100

    Modified tests so that Dd1 has let and where clause variables that have the same name as the variable being deleted.

--- test/testdata/DeleteDef/Dd1.hs
+++ test/testdata/DeleteDef/Dd1.hs
@@ -8,3 +8,11 @@ remove5s :: [Integer] -> [Integer]
 remove5s [] = []
 remove5s (5:xs) = remove5s xs
 remove5s (x:xs) =  x: (remove5s xs)
+
+funcWithLet :: (Num a) => a -> a
+funcWithLet x = let c = 12 in
+  c * x
+
+funcWithWhere :: (Num a) => a -> a
+funcWithWhere x = x + c
+  where c = 12
