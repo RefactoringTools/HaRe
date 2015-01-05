b5b5572818591081b1fcba7b3ca0ef6df8b20893
Alan Zimmerman <alan.zimm@gmail.com>
2015-01-05 11:52:46 +0200

    New file

---
+++ src/Language/Haskell/Refact/Utils/Types.hs
@@ -0,0 +1,46 @@
+module Language.Haskell.Refact.Utils.Types
+       (
+         mainTid
+       , TokenCache(..)
+
+       , ghead
+       , glast
+       , gtail
+       , gfromJust
+       ) where
+
+import qualified Data.Map as Map
+
+-- ---------------------------------------------------------------------
+
+data TreeId = TId !Int deriving (Eq,Ord,Show)
+
+-- |Identifies the tree carrying the main tokens, not any work in
+-- progress or deleted ones
+mainTid :: TreeId
+mainTid = TId 0
+
+data TokenCache a = TK
+  { tkCache :: !(Map.Map TreeId a)
+  , tkLastTreeId :: !TreeId
+  }
+
+-- ---------------------------------------------------------------------
+-- Putting these here for the time being, to avoid import loops
+
+ghead :: String -> [a] -> a
+ghead  info []    = error $ "ghead "++info++" []"
+ghead _info (h:_) = h
+
+glast :: String -> [a] -> a
+glast  info []    = error $ "glast " ++ info ++ " []"
+glast _info h     = last h
+
+gtail :: String -> [a] -> [a]
+gtail  info []   = error $ "gtail " ++ info ++ " []"
+gtail _info h    = tail h
+
+gfromJust :: [Char] -> Maybe a -> a
+gfromJust _info (Just h) = h
+gfromJust  info Nothing = error $ "gfromJust " ++ info ++ " Nothing"
+
