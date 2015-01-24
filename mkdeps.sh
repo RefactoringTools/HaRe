9afaeaaaa3d858d6b604373c870a7d2b7b5a997d
Alan Zimmerman <alan.zimm@gmail.com>
2015-01-24 17:43:02 +0200

    More travis tweaks

--- mkdeps.sh
+++ mkdeps.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-#(cd ../ghc-syb/utils                && cabal clean && cabal install --allow-newer)
-(cd ../hslogger                     && cabal clean && cabal install --allow-newer)
-#(cd ../../DanielG/ghc-mod           && cabal clean && cabal install --allow-newer)
+(cd ../../alanz/hslogger            && cabal clean && cabal install --allow-newer)
 (cd ../../alanz/ghc-mod             && cabal clean && cabal install --allow-newer)
-(cd ../ghc-exactprint               && cabal clean && cabal install --allow-newer)
 
 cabal clean && cabal install   --allow-newer --enable-tests --dependencies-only
 cabal clean && cabal configure --allow-newer --enable-tests
