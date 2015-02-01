800b9de22f779b6bc7334d4938b837448074fe06
Alan Zimmerman <alan.zimm@gmail.com>
2015-02-01 10:32:47 +0200

    --allow-newer breaks ghc-mod

--- mkdeps.sh
+++ mkdeps.sh
@@ -3,8 +3,10 @@
 (cd ../../alanz/hslogger            && cabal clean && cabal install --allow-newer)
 (cd ../../alanz/ghc-mod             && cabal clean && cabal install --allow-newer)
 
-cabal clean && cabal install   --allow-newer --enable-tests --dependencies-only
-cabal clean && cabal configure --allow-newer --enable-tests
+#cabal clean && cabal install   --allow-newer --enable-tests --dependencies-only
+cabal clean && cabal install    --enable-tests --dependencies-only
+#cabal clean && cabal configure --allow-newer --enable-tests
+cabal clean && cabal configure  --enable-tests
 
 # Make sure the tests run against the newly installed environment
 (cd test/testdata && cabal clean && cabal configure)
