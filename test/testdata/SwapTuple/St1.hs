9fde6aa7b2b576e47189660bc8a2d41f387457b8
Stephen A <StephenAdams.Minn@gmail.com>
2015-02-11 23:50:56 +0000

    Started making test cases for tuple swap refactoring.

---
+++ test/testdata/SwapTuple/St1.hs
@@ -0,0 +1,29 @@
+module St1 where
+   import Control.Concurrent
+
+   type Foo = (String, Int)
+
+   mkFoo :: Int -> String -> Foo
+   mkFoo i s = (s,i)
+   
+   getStr :: Foo -> String
+   getStr foo = fst foo
+
+   getInt :: Foo -> Int
+   getInt (_,i) = i
+
+   inc :: Foo -> Foo
+   inc (s,i) = (s,i+1)
+
+   dec :: Foo -> Foo
+   dec (s,i) = (s, i-1)
+
+   countDown :: Foo -> IO ()
+   countDown foo = do
+      putStrLn $ fst foo
+      if (snd foo) <= 0 
+         then 
+            putStrLn "Exiting..."
+         else do
+            threadDelay 1000000
+            countDown $ dec foo
