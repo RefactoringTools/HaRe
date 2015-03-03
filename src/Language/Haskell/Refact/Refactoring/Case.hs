7ecdf8dc8a34b93e0b87fdd8568aaf8892ba5b51
Alan Zimmerman <alan.zimm@gmail.com>
2015-03-03 22:37:54 +0200

    First test for ifToCase passes

--- src/Language/Haskell/Refact/Refactoring/Case.hs
+++ src/Language/Haskell/Refact/Refactoring/Case.hs
@@ -92,10 +92,10 @@ ifToCaseTransform e@(GHC.L l (GHC.HsIf _se e1 e2 e3)) = do
                    GHC.L trueLoc1 $ GHC.ConPatIn (GHC.L trueLoc trueName) (GHC.PrefixCon [])
                  ]
                  Nothing
-                 ((GHC.GRHSs
+                 (GHC.GRHSs
                    [
                      GHC.L trueRhsLoc $ GHC.GRHS [] e2
-                   ] GHC.EmptyLocalBinds))
+                   ] GHC.EmptyLocalBinds)
                 )
               , (GHC.L falseMatchLoc $ GHC.Match
                  Nothing
@@ -103,14 +103,14 @@ ifToCaseTransform e@(GHC.L l (GHC.HsIf _se e1 e2 e3)) = do
                    GHC.L falseLoc1 $ GHC.ConPatIn (GHC.L falseLoc falseName) (GHC.PrefixCon [])
                  ]
                  Nothing
-                 ((GHC.GRHSs
+                 (GHC.GRHSs
                    [
                      GHC.L falseRhsLoc $ GHC.GRHS [] e3
-                   ] GHC.EmptyLocalBinds))
+                   ] GHC.EmptyLocalBinds)
                 )
               ] [] GHC.placeHolderType GHC.FromSource))
   --(_ret2,anne) <- resequenceAnnotations ret
-  logm $ "annGetConstr\n\n:" ++ show (annGetConstr trueName)
+  -- logm $ "annGetConstr\n\n:" ++ show (annGetConstr trueName)
 
   let annf =  Map.union (Map.fromList $ map (\(k, v) -> (uncurry AnnKey k, annNone { anns = v}))
               [((caseLoc,     CN "HsCase"), [ (G GHC.AnnCase, DP (0,1))
@@ -121,34 +121,34 @@ ifToCaseTransform e@(GHC.L l (GHC.HsIf _se e1 e2 e3)) = do
               ,((falseLoc,    CN "Unqual"), [(G GHC.AnnVal,    DP (1,0))])
               ,((falseRhsLoc, CN "GRHS"),   [(G GHC.AnnRarrow, DP (0,1))])
               ]) Map.empty
-  logm $ "\n\n\n" ++ showGhc annf
-  let anne2 = setOffsets annf [ ( AnnKey caseLoc       (CN "HsCase"),   (DP (0,1),LineSame, 0, 2) )
-                              , ( AnnKey trueRhsLoc    (CN "GRHS"),     (DP (0,2),LineSame, 0, 6) )
-                              , ( AnnKey trueMatchLoc  (CN "Match"),    (DP (1,4),LineChanged, 0, 4) )
-                              , ( AnnKey falseRhsLoc   (CN "GRHS"),     (DP (0,1),LineSame, 0, 6) )
-                              , ( AnnKey falseMatchLoc (CN "Match"),    (DP (1,4),LineChanged, 0, 4) )
-                              , ( AnnKey trueLoc1      (CN "ConPatIn"), (DP (1,0),LineSame, 0, 0) )
-                              , ( AnnKey trueLoc       (CN "Unqual"),   (DP (1,0),LineSame, 0, 0) )
-                              , ( AnnKey falseLoc1     (CN "ConPatIn"), (DP (1,0),LineSame, 0, 0) )
-                              , ( AnnKey falseLoc      (CN "Unqual"),   (DP (1,0),LineSame, 0, 0))
+  -- logm $ "\n\n\n" ++ showGhc annf
+  let anne2 = setOffsets annf [ ( AnnKey caseLoc       (CN "HsCase"),   (DP (0,1),LineSame, 9, 2) )
+                              , ( AnnKey trueRhsLoc    (CN "GRHS"),     (DP (0,2),LineSame, 19, 6) )
+                              , ( AnnKey trueMatchLoc  (CN "Match"),    (DP (1,4),LineChanged, 13, 13) )
+                              , ( AnnKey trueLoc1      (CN "ConPatIn"), (DP (1,0),LineSame, 13, 0) )
+                              , ( AnnKey trueLoc       (CN "Unqual"),   (DP (1,0),LineSame, 13, 0) )
+                              , ( AnnKey falseRhsLoc   (CN "GRHS"),     (DP (0,2),LineSame, 19, 6) )
+                              , ( AnnKey falseMatchLoc (CN "Match"),    (DP (1,4),LineChanged, 13, 13) )
+                              , ( AnnKey falseLoc1     (CN "ConPatIn"), (DP (1,0),LineSame, 13, 0) )
+                              , ( AnnKey falseLoc      (CN "Unqual"),   (DP (1,0),LineSame, 13, 0))
                               ]
 
-  logm $ "\n\n\nanne2" ++ showGhc anne2
+  -- logm $ "\n\n\nanne2" ++ showGhc anne2
 
   oldAnns <- getRefactAnns
   let anne1 = Map.delete (AnnKey l (CN "HsIf")) oldAnns
       final = mergeAnns anne1 anne2
       anne3 = setLocatedOffsets final
-                [ (e1, (DP (0,1),LineSame,0,5))
-                , (e2, (DP (0,1),LineSame,0,5))
-                , (e3, (DP (0,1),LineSame,0,5))
+                [ (e1, (DP (0,1),LineSame,14,5))
+                , (e2, (DP (0,1),LineSame,22,3))
+                , (e3, (DP (0,1),LineSame,22,3))
                 ]
   -- let anne3 = setLocatedDp final e2 (DP (0,1)) 5
   -- let anne4 = setLocatedDp anne3 e3 (DP (0,1)) 0
   -- let anne5 = setLocatedDp anne4 e1 (DP (0,1)) 5
   -- let out = (setColRec (const 0) ret) . setColRec (const 0) e $ anne5
   -- logm $ "Case:fi=" ++ out
-  logm $ "Case:anne5=" ++ showGhc anne3
+  -- logm $ "Case:anne5=" ++ showGhc anne3
   -- setRefactAnns out
   setRefactAnns anne3
   return ret
