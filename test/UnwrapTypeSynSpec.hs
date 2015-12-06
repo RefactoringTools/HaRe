dbe90c2b0d1dce0a32a1401ccfc594938628073e
Stephen Adams <sa597@kent.ac.uk>
2015-12-06 15:00:50 +0000

    Added test case for unwrap type synonym.

--- test/UnwrapTypeSynSpec.hs
+++ test/UnwrapTypeSynSpec.hs
@@ -12,9 +12,16 @@ spec :: Spec
 spec = do
   describe "doUnwrapTypeSyn" $ do
     it "Removes a simply synonym for String" $ do
-      res <- ct $ unwrapTypeSyn logTestSettings testOptions "./UnwrapTypeSyn/UTS1.hs" (3,6) "Foo"
+      res <- ct $ unwrapTypeSyn defaultTestSettings testOptions "./UnwrapTypeSyn/UTS1.hs" (3,6) "Foo"
       res' <- ct $ mapM makeRelativeToCurrentDirectory res
       res' `shouldBe` ["UnwrapTypeSyn/UTS1.hs"]
       diff <- ct $ compareFiles "./UnwrapTypeSyn/UTS1.refactored.hs"
                            "./UnwrapTypeSyn/UTS1.hs.expected"
+      diff `shouldBe` []
+    it "Removes a more complicated tuple type" $ do
+      res <- ct $ unwrapTypeSyn logTestSettings testOptions "./UnwrapTypeSyn/UTS2.hs" (3,6) "Foo"
+      res' <- ct $ mapM makeRelativeToCurrentDirectory res
+      res' `shouldBe` ["UnwrapTypeSyn/UTS2.hs"]
+      diff <- ct $ compareFiles "./UnwrapTypeSyn/UTS2.refactored.hs"
+              "./UnwrapTypeSyn/UTS2.hs.expected"
       diff `shouldBe` []
