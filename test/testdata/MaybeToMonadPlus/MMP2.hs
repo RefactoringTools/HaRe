6846cb0c287f4b1fff9a4a96cd7713485d5a6102
Stephen Adams <sa597@kent.ac.uk>
2015-12-16 14:51:08 +0000

    Some changes to tests for maybe to monad plus.

--- test/testdata/MaybeToMonadPlus/MMP2.hs
+++ test/testdata/MaybeToMonadPlus/MMP2.hs
@@ -3,7 +3,7 @@ module MMP2 where
 f :: Int -> Maybe String
 f i =
   if (i == 0)
-    then return (show i)
+    then (Just (show i))
     else Nothing
 
 f2 :: (Show a) => Maybe a -> IO ()
@@ -11,4 +11,3 @@ f2 m =
   case m of
   Nothing -> putStrLn "Something went wrong"
   (Just i) -> putStrLn $ "The result is: " ++ (show i)
-  
